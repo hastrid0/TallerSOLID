8e216c6ecca0767c13173099d043dfda6090d381
Betancourt1996 <braybeta@espol.edu.ec>
2020-11-11 17:44:50 -0500

    eleminado codigo repetido de subclases Pastel

--- TallerSOLID/src/Postres/Pastel.java
+++ TallerSOLID/src/Postres/Pastel.java
@@ -21,18 +21,11 @@ public class Pastel extends Postre{
         this.precioParcial = 15.55;
     }
     
-    public double calcularPrecioFinal(){
-        double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
-        return precioFinal;
-    }
-
+ 
     @Override
     public String toString() {
         return "Pastel{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
     }
-    public String showPrecioFinal(){
-        return "Precio Final: $ " + calcularPrecioFinal();
-    }
+    
     
 }
