db487909297e8224753ca50b77aec7af3a697573
djsabando <douglas_sabando@hotmail.es>
2020-11-11 20:43:48 -0500

    Main adaptado a funciones

--- TallerSOLID/src/Principal/Sistema.java
+++ TallerSOLID/src/Principal/Sistema.java
@@ -8,41 +8,52 @@ import Adicionales.*;
 import Postres.*;
 import Procesos.*;
 import Leche.*;
+import java.util.ArrayList;
 /**
  *
  * @author djurado
  */
 public class Sistema {
     public static void main(String [ ] args){
+        
         // Producir un helado de vainilla y una torta de chocolate, 
         // a ambos agregarles CREMA y FRUTILLAS
         // y cambiar el tipo de leche por Leche Descremada
         // Finalmente mostrar el precio final de cada uno
+                
         LecheEntera leche = new LecheDeslactosada();
         ManejadorDeLeche mnj_leche = new ManejadorDeLeche();
-           
+        ArrayList<Postre> arrPostres = new ArrayList<>();
+        
+          
         // Producir Helado
         Helado helado_vainilla = new Helado("Vainilla");
         CREMA c= new CREMA();
         c.setNombre();
         FRUTILLA f=new FRUTILLA();
         f.setNombre();
-        helado_vainilla.anadirAderezo(c);
-        helado_vainilla.anadirAderezo(f);
         System.out.println(helado_vainilla);
-        mnj_leche.cambiarTipoLeche(leche, helado_vainilla);
-        System.out.println(ManejeoDePrecio.showPrecioFinal(helado_vainilla));
-        //System.out.println(helado_vainilla.showPrecioFinal());
+        arrPostres.add(helado_vainilla);
         
+                //System.out.println(helado_vainilla.showPrecioFinal());
         // Producir Pastel
         Pastel pastel_chocolate = new Pastel("Chocolate");
-        pastel_chocolate.anadirAderezo(c);
-        pastel_chocolate.anadirAderezo(f);
         System.out.println(pastel_chocolate);
-        mnj_leche.cambiarTipoLeche(leche, pastel_chocolate);
-        System.out.println(ManejeoDePrecio.showPrecioFinal(pastel_chocolate));
+        arrPostres.add(pastel_chocolate);
         //System.out.println(helado_vainilla.showPrecioFinal());
         
+        arrPostres.forEach(postre -> {
+            postre.anadirAderezo(c);
+            postre.anadirAderezo(f);
+            System.out.println(postre);
+            mnj_leche.cambiarTipoLeche(leche, postre);
+            System.out.println(ManejeoDePrecio.showPrecioFinal(postre));
+        });
         
+                
     }
-}
+                
+  }
+
+    
+    
